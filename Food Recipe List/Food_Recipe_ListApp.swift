c8af138ba4bf5087fbe0f40ca9d77ffa25973740
deir905 <deir905@aucklanduni.ac.nz>
2022-07-20 17:27:35 +1200

    Initial Commit

---
+++ Food Recipe List/Food_Recipe_ListApp.swift
@@ -0,0 +1,17 @@
+//
+//  Food_Recipe_ListApp.swift
+//  Food Recipe List
+//
+//  Created by Daniel Eir on 20/07/22.
+//
+
+import SwiftUI
+
+@main
+struct Food_Recipe_ListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
